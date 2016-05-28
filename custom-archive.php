1d746d51a8cb82920f0b3332a3c3dfb295d05be6
Harvey Zack <laozhaochaguan@hotmail.com>
2016-05-28 08:44:05 +0800

    update

--- custom-archive.php
+++ custom-archive.php
@@ -91,41 +91,55 @@ $bulletin = cs_get_option( 'i_bulletin' );
         									<div class="post-wrap">
         										<div class="post-inner">
         											<div class="post-body">
-								<header>
-									<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-									<ul class="top_meta"></ul>
-								</header>
-        												</header>
-        												<div class="post-content">
-        													<div calss="content">
-                                                                <div class="archivePost">
-                                                                     <ul class="timeline">
-                                                                     <li class="tl-header">
-                                                                       <div class="btn btn-info">现在</div>
-                                                                     </li>
-                                                                     <?php $count_posts = wp_count_posts(); $published_posts = $count_posts->publish;
-                                                                     query_posts( 'posts_per_page=-1' );
-                                                                     while ( have_posts() ) : the_post();
-                                                                         echo '<li class="tl-item"><div class="tl-wrap">';
-                                                                         echo '<span class="tl-date">';
-                                                                         the_time(get_option( 'date_format' ));
-                                                                         echo '</span><div class="tl-content">
-                                                                         <span class="arrow"></span>
-                                                                         <a href="';
-                                                                         the_permalink();
-                                                                         echo '" title="'.esc_attr( get_the_title() ).'">';
-                                                                         the_title();
-                                                                         echo '</a></div></div></li>';
-                                                                         $published_posts--;
-                                                                     endwhile;
-                                                                     wp_reset_query(); ?>
-                                                                      <li class="tl-header">
-                                                                        <div class="btn btn-info">过去</div>
-                                                                      </li>
-                                                                     </ul>
-                                                                 </div>
-        													</div>
-        												</div>
+								<div class="content">
+									<?php the_content(__( 'Read More','island')); ?>
+									<div id="archives">      
+										<div id="archives-content">      
+										<?php       
+											$the_query = new WP_Query( 'posts_per_page=-1&ignore_sticky_posts=1' );      
+											$year=0; $mon=0; $i=0; $j=0;      
+											$all = array();      
+											$output = '';      
+											while ( $the_query->have_posts() ) : $the_query->the_post();      
+												$year_tmp = get_the_time('Y');      
+												$mon_tmp = get_the_time('n');      
+												$y=$year; $m=$mon;      
+												if ($mon != $mon_tmp && $mon > 0) $output .= '</div></div>';      
+												if ($year != $year_tmp) { // 输出年份      
+													$year = $year_tmp;      
+													$all[$year] = array();      
+												}      
+												if ($mon != $mon_tmp) { // 输出月份      
+													$mon = $mon_tmp;      
+													array_push($all[$year], $mon);      
+													$output .= "<div class='archive_title' id='arti-$year-$mon'><h3>$year-$mon</h3><div class='archives archives-$mon' data-date='$year-$mon'>";      
+												}      
+												$output .= '<div class="brick"><a href="'.get_permalink() .'"><span class="time">'.get_the_time('n-d').'</span>'.get_the_title() .'<em>('. get_comments_number('0', '1', '%') .')</em></a></div>';      
+											endwhile;      
+											wp_reset_postdata();      
+											$output .= '</div></div>';      
+											echo $output;      
+										 
+											$html = "";      
+											$year_now = date("Y");      
+											foreach($all as $key => $value){// 输出 左侧年份表      
+												$html .= "<li class='year' id='year-$key'><a href='#' class='year-toogle' id='yeto-$key'>$key</a><ul class='monthall'>";      
+												for($i=12; $i>0; $i--){      
+													if($key == $year_now && $i > $value[0]) continue;      
+													$html .= in_array($i, $value) ? ("<li class='month monthed' id='mont-$key-$i'>$i</li>") : ("<li class='month'>$i</li>");      
+												}      
+												$html .= "</ul></li>";   
+											}      
+										?>     
+										</div>   
+										<?php if (!is_mobile()) { ?>
+											<div id="archive-nav" class="fade out">      
+												<ul class="archive-nav"><?php echo $html;?></ul>      
+											</div>  
+										<?php }?>	
+									</div><!-- #archives -->  										
+								</div>
+							</div>
         											</div>
         										</div>
         									</div>
