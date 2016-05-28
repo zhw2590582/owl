1a28eb6dcb56009912adf8337c45617e999e98fd
Harvey Zack <laozhaochaguan@hotmail.com>
2016-05-28 09:06:30 +0800

    update

---
+++ tab.php
@@ -0,0 +1,7 @@
+<div class="tabs_title_box">
+    <ul class="tabs_title clearfix">
+        <li><a href="#"><i class="fa fa-comments-o"></i></a></li>
+        <li><a href="#"><i class="fa fa-clock-o"></i></a></li>
+        <li><a href="#"><i class="fa fa-fire"></i></a></li>
+    </ul>
+</div>
