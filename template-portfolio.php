f76b677671fdfcc1df6b96f2ca29db1f79095373
Harvey Zack <laozhaochaguan@hotmail.com>
2016-05-28 11:14:19 +0800

    update

--- template-portfolio.php
+++ template-portfolio.php
@@ -6,16 +6,16 @@ register_post_type(
 		'publicly_queryable' => true,
 		'hierarchical' => false,
 		'labels'=>array(
-			'name' => _x('作品', 'post type general name'),
-			'singular_name' => _x('作品', 'post type singular name'),
-			'add_new' => _x('添加新作品', '作品'),
-			'add_new_item' => __('添加新作品'),
-			'edit_item' => __('编辑作品'),
-			'new_item' => __('新的作品'),
-			'view_item' => __('预览作品'),
-			'search_items' => __('搜索作品'),
-			'not_found' =>  __('您还没有发布作品'),
-			'not_found_in_trash' => __('回收站中没有作品'), 
+			'name' => _x('图集', 'post type general name'),
+			'singular_name' => _x('图集', 'post type singular name'),
+			'add_new' => _x('添加新图集', '图集'),
+			'add_new_item' => __('添加新图集'),
+			'edit_item' => __('编辑图集'),
+			'new_item' => __('新的图集'),
+			'view_item' => __('预览图集'),
+			'search_items' => __('搜索图集'),
+			'not_found' =>  __('您还没有发布图集'),
+			'not_found_in_trash' => __('回收站中没有图集'),
 			'parent_item_colon' => ''
 			),
 		 'show_ui' => true,
@@ -40,15 +40,15 @@ add_action('init', 'post_type_work');
 
 function create_genre_taxonomy() {
   $labels = array(
-		 'name' => _x( '作品分类', 'taxonomy general name' ),
+		 'name' => _x( '图集分类', 'taxonomy general name' ),
 		 'singular_name' => _x( 'genre', 'taxonomy singular name' ),
 		 'search_items' =>  __( '搜索分类' ),
 		 'all_items' => __( '全部分类' ),
 		 'parent_item' => __( '父级分类目录' ),
 		 'parent_item_colon' => __( '父级分类目录:' ),
-		 'edit_item' => __( '编辑作品分类' ), 
+		 'edit_item' => __( '编辑图集分类' ),
 		 'update_item' => __( '更新' ),
-		 'add_new_item' => __( '添加新作品分类' ),
+		 'add_new_item' => __( '添加新图集分类' ),
 		 'new_item_name' => __( 'New Genre Name' ),
   ); 
   register_taxonomy('genre',array('work'), array(
