2674b3800a512907726a5c348f918cb242317bcd
Harvey Zack <laozhaochaguan@hotmail.com>
2016-05-28 09:07:54 +0800

    update

--- tab.php
+++ tab.php
@@ -5,3 +5,18 @@
         <li><a href="#"><i class="fa fa-fire"></i></a></li>
     </ul>
 </div>
+<div class="tab_content">
+    <div class="tabs_item" id="recent-comment">
+        <ul>
+            <?php h_comments($outer='博主',$limit= 10);?>
+        </ul>
+    </div>
+    <div class="tabs_item hide" id="recent-post">
+
+    </div>
+    <div class="tabs_item hide" id="hot-post">
+        <ul class="comm_posts">
+            <?php if(function_exists('most_comm_posts')) most_comm_posts(120,12); ?>
+        </ul>
+    </div>
+</div>
